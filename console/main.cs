76619a0ee6d15ccf33686a3af29731089ef232b5
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-05-13 01:18:18 +1000

    Moved console scripts into actual script file.

--- console/main.cs
+++ console/main.cs
@@ -24,3 +24,85 @@ exec("./profiles.cs");
 exec("./console.gui");
 
 GlobalActionMap.bind("keyboard", "tilde", "toggleConsole");
+
+function ConsoleEntry::eval()
+{
+   %text = trim(ConsoleEntry.getValue());
+   if(%text $= "")
+      return;
+
+   // If it's missing a trailing () and it's not a variable,
+   // append the parentheses.
+   if(strpos(%text, "(") == -1 && !isDefined(%text)) {
+      if(strpos(%text, "=") == -1 && strpos(%text, " ") == -1) {
+         if(strpos(%text, "{") == -1 && strpos(%text, "}") == -1) {
+            %text = %text @ "()";
+         }
+      }
+   }
+
+   // Append a semicolon if need be.
+   %pos = strlen(%text) - 1;
+   if(strpos(%text, ";", %pos) == -1 && strpos(%text, "}") == -1) {
+      %text = %text @ ";";
+   }
+
+   // Turn off warnings for assigning from void
+   // and evaluate the snippet.
+   if(!isDefined("$Con::warnVoidAssignment"))
+      %oldWarnVoidAssignment = true;
+   else
+      %oldWarnVoidAssignment = $Con::warnVoidAssignment;
+   $Con::warnVoidAssignment = false;
+
+   echo("==>" @ %text);
+   if(   !startsWith(%text, "function ")
+       && !startsWith(%text, "datablock ")
+       && !startsWith(%text, "foreach(")
+       && !startsWith(%text, "foreach$(")
+       && !startsWith(%text, "if(")
+       && !startsWith(%text, "while(")
+       && !startsWith(%text, "for(")
+       && !startsWith(%text, "switch(")
+       && !startsWith(%text, "switch$("))
+      eval("%result = " @ %text);
+   else
+      eval(%text);
+   $Con::warnVoidAssignment = %oldWarnVoidAssignment;
+
+   ConsoleEntry.setValue("");
+
+   // Echo result.
+   if(%result !$= "")
+      echo(%result);
+}
+
+function ToggleConsole(%make)
+{
+   if (%make) {
+      if (ConsoleDlg.isAwake()) {
+         // Deactivate the console.
+         Canvas.popDialog(ConsoleDlg);
+      } else {
+         Canvas.pushDialog(ConsoleDlg, 99);         
+      }
+   }
+}
+
+function ConsoleDlg::hideWindow(%this)
+{
+   %this-->Scroll.setVisible(false);
+}
+
+function ConsoleDlg::showWindow(%this)
+{
+   %this-->Scroll.setVisible(true);
+}
+
+function ConsoleDlg::setAlpha( %this, %alpha)
+{
+   if (%alpha $= "")
+      ConsoleScrollProfile.fillColor = $ConsoleDefaultFillColor;
+   else
+      ConsoleScrollProfile.fillColor = getWords($ConsoleDefaultFillColor, 0, 2) SPC %alpha * 255.0;
+}
