5c5be7b9671f10965277984693830a41568f45f4
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-05-13 01:21:07 +1000

    Started work on next tutorial.

---
+++ tutorials/03_add_a_player/main.cs
@@ -0,0 +1,104 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2012 GarageGames, LLC
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//-----------------------------------------------------------------------------
+
+exec("./playGui.gui");
+
+datablock CameraData(Observer) {};
+
+singleton Material(PlayerMaterial) {
+   diffuseColor[0] = "1 0 0";
+   mapTo = "PlayerTexture";
+};
+
+singleton Material(BlankWhite) {
+   detailMap[0] = "./white";
+   detailScale[0] = "20 20";
+};
+
+datablock PlayerData(BoxPlayer) {
+   shapeFile = "./player.dts";
+   cameraMaxDist = 5;
+   jumpDelay = 0;
+};
+
+//-----------------------------------------------------------------------------
+function GameConnection::onEnterGame(%client) {
+   new Player(ThePlayer) {
+      datablock = BoxPlayer;
+      position = "0 0 1";
+   };
+   %client.setControlObject(ThePlayer);
+   GameGroup.add(ThePlayer);
+
+   Canvas.setContent(PlayGui);
+   activateDirectInput();
+}
+
+//-----------------------------------------------------------------------------
+function onStart() {
+   new SimGroup(GameGroup) {
+      new LevelInfo(TheLevelInfo) {
+         canvasClearColor = "0 0 0";
+      };
+      new GroundPlane(TheGround) {
+         position = "0 0 0";
+         material = BlankWhite;
+      };
+      new Sun(TheSun) {
+         azimuth = 230;
+         elevation = 45;
+         color = "1 1 1";
+         ambient = "0.1 0.1 0.1";
+         castShadows = true;
+      };
+   };
+
+   GlobalActionMap.bind("keyboard", "escape", "quit");
+
+   GlobalActionMap.bindCmd("keyboard", "w", "$mvForwardAction = 1;",  "$mvForwardAction = 0;");
+   GlobalActionMap.bindCmd("keyboard", "s", "$mvBackwardAction = 1;", "$mvBackwardAction = 0;");
+   GlobalActionMap.bindCmd("keyboard", "a", "$mvLeftAction = 1;",     "$mvLeftAction = 0;");
+   GlobalActionMap.bindCmd("keyboard", "d", "$mvRightAction = 1;",    "$mvRightAction = 0;");
+   GlobalActionMap.bindCmd("keyboard", "space", "$mvTriggerCount2++;", "");
+   GlobalActionMap.bind("mouse", "xaxis", "yaw");
+   GlobalActionMap.bind("mouse", "yaxis", "pitch");
+
+   GlobalActionMap.bindCmd("keyboard", "tab", "ServerConnection.setFirstPerson(!ServerConnection.isFirstPerson());", "");
+}
+
+function yaw(%amount) {
+   $mvYaw += %amount * 0.01;
+}
+
+function pitch(%amount) {
+   $mvPitch += %amount * 0.01;
+}
+
+//-----------------------------------------------------------------------------
+function onExit() {
+   GameGroup.delete();
+
+   ServerConnection.delete();
+   ServerGroup.delete();
+
+   deleteDataBlocks();
+}
